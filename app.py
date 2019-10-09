208b241bc36bdc1320dd99ae9a54b84ad2339cc0
Hamilton Kamiya <31168921+enghamilton@users.noreply.github.com>
2019-10-09 16:00:57 -0300

    Update app.py
    added requests

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-"""Flask App Project."""
+"""Flask App Project.
 
 from flask import Flask, jsonify
 app = Flask(__name__)
@@ -12,4 +12,21 @@ def index():
 
 
 if __name__ == '__main__':
+    app.run()
+
+"""
+from flask import Flask
+import requests
+
+#x = requests.get('https://w3schools.com/python/demopage.htm')
+x = requests.get('http://pizzaria2.000webhostapp.com/android_connect/get_all_products.php')
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    #return "Hello World!"
+	return x.text
+
+if __name__ == "__main__":
     app.run()
