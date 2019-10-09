dff38d9e94eee4e5652a80cc0e85a90daf312010
Hamilton Kamiya <31168921+enghamilton@users.noreply.github.com>
2019-10-09 16:03:31 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-"""Flask App Project.
+"""Flask App Project."""
 
 from flask import Flask, jsonify
 app = Flask(__name__)
@@ -7,26 +7,9 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     """Return homepage."""
-    json_data = {'Hello': 'World!'}
+    json_data = {'Hello': 'World!','pid': '01','pid': '02','pid': '03' }
     return jsonify(json_data)
 
 
 if __name__ == '__main__':
-    app.run()
-
-"""
-from flask import Flask
-import requests
-
-#x = requests.get('https://w3schools.com/python/demopage.htm')
-x = requests.get('http://pizzaria2.000webhostapp.com/android_connect/get_all_products.php')
-
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-    #return "Hello World!"
-	return x.text
-
-if __name__ == "__main__":
     app.run()
