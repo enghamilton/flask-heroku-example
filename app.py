64c5ef50322383389cc4ba6e8bfe9118e57cbade
Hamilton Kamiya <31168921+enghamilton@users.noreply.github.com>
2019-11-20 19:07:40 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,29 @@
 """Flask App Project."""
 
+import requests
 from flask import Flask, jsonify
 app = Flask(__name__)
 
+USERNAME = "enghamilton"
+#PASSWORD = "p4dcGj97"
+
+LOGIN_URL = "https://github.com/login"
+URL = "https://github.com/"
 
 @app.route('/')
 def index():
-    """Return homepage."""
-    json_data = { "products":[{"pid":"1","name":"Pizza Portuguesa1","price":"23.50","description":"Queijo mussarela, presunto, ovos, ervilhas, palmito, cebola, oregano, azeitona."},{"pid":"2","name":"pizza 2","price":"23.99","description":"ingrediente 2"},{"pid":"3","name":"pizza 1","price":"23.00","description":"ingrediente 1"},{"pid":"4","name":"pizza 3","price":"12.00","description":"ingrediente da pizza 3"}],"success":1 }
-    return jsonify(json_data)
+    # Create payload
+    payload = {
+        "login": USERNAME, 
+        "password": PASSWORD
+    }
 
+	#https ://pybit.es/requests-session.html
+    with requests.Session() as session:
+		post = session.post(LOGIN_URL, data=payload)
+		r = session.get(URL)
+    
+    return "%s" % r.content
 
 if __name__ == '__main__':
     app.run()
