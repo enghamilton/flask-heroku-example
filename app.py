5c038c61cf24852f35a6ea9e914b7b4d31c47b4a
Hamilton Kamiya <31168921+enghamilton@users.noreply.github.com>
2019-11-20 16:43:07 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     """Return homepage."""
-    json_data = {'Hello': 'World!'}
+    json_data = {'products':[{'pid':1,'username':'user 01'},{'pid':2,'username':'user 02'},{'pid':3,'username':'user 03'}],'success':1}
     return jsonify(json_data)
 
 
