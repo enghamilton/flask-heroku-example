c8e038d310b8a8e5a9be52f09c00eaca263689c8
Hamilton Kamiya <31168921+enghamilton@users.noreply.github.com>
2019-11-20 19:25:21 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,13 +5,17 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 USERNAME = "enghamilton"
-PASSWORD = "p4dcGj97"
+PASSWORD = "*******7"
 
 LOGIN_URL = "https://github.com/login"
 URL = "https://github.com/"
 
 @app.route('/')
 def index():
+    return "Hello World"
+
+@app.route('/scraper')
+def scraper():
     # Create payload
     payload = {
         "login": USERNAME, 
@@ -24,6 +28,7 @@ def index():
 		r = session.get(URL)
     
     return "%s" % r.content
+
 
 if __name__ == '__main__':
     app.run()
